30d849b4a27a0a77236a5c134902cf375a5dcb67
Oscar13XD <hernandezmahechaoscar0mail.com>
2022-05-04 18:02:57 -0500

    prueba repo 4

--- Controllers/AdministradorController.cs
+++ Controllers/AdministradorController.cs
@@ -414,6 +414,12 @@ namespace GoodDriving.Controllers
             int b = 6;
             return a + b;
         }
+        public int sumar2()
+        {
+            int a = 5;
+            int b = 6;
+            return a + b;
+        }
         struct strUsuario
         {
             public int Id { get; set; }
