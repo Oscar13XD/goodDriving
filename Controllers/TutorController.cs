3bd988510d6dc4f6d43c494a6bb67fc5ed08cc5a
Oscar13XD <hernandezmahechaoscar0mail.com>
2022-05-05 16:18:16 -0500

    Ver Horario Tutor

--- Controllers/TutorController.cs
+++ Controllers/TutorController.cs
@@ -1,6 +1,7 @@
 ﻿using GoodDriving.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GoodDriving.Controllers
 {
@@ -18,5 +19,46 @@ namespace GoodDriving.Controllers
             return View();
         }
 
+        public IActionResult VerHorarioTeorico()
+        {
+            return View();
+        }
+
+        [HttpGet] //LEER USUARIOS
+        public async Task<IActionResult> TraerHorarioTutorTeorico(int id)
+        {
+            List<HorarioTutor> HorarioTutors = await _context.HorarioTutors.Where(x => x.IdTutor == id).ToListAsync();
+            if(HorarioTutors.Count > 0)
+            {
+                List<strHorarioTutor> strHorarioTutors = new List<strHorarioTutor>();
+                foreach (HorarioTutor horarioTutor in HorarioTutors)
+                {
+                    //horarioTutor.Nombre1 += " " + horarioTutor.Nombre2;
+                    //horarioTutor.Apellido1 += " " + horarioTutor.Apellido2;
+                    strHorarioTutor str = new strHorarioTutor();
+                    str.Id = horarioTutor.Id;
+                    str.Dia = horarioTutor.Dia;
+                    str.Hora = horarioTutor.Hora;
+                    str.Cupo = horarioTutor.Cupo;
+                    strHorarioTutors.Add(str);
+                }
+                return Json(new { horarioTutors = strHorarioTutors });
+            }
+            return Content("no hay");
+        }
+
+
+        struct strHorarioTutor
+        {
+            public int Id { get; set; }
+            public int? IdTutor { get; set; }
+            public string Nombre1Tutor { get; set; }
+            public string Nombre2Tutor { get; set; }
+            public string Apellido1Tutor { get; set; }
+            public string Apellido2Tutor { get; set; }
+            public string? Dia { get; set; }
+            public string? Hora { get; set; }
+            public int? Cupo { get; set; }
+        }
     }
 }
